20b9cf11ffa1003b1273a4c38e9c8a7de044a781
pujachoudhari1 <129393847+pujachoudhari1@users.noreply.github.com>
2023-05-05 17:02:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Engineer", "Developer"],
+        strings: ["Software Engineer", "Java-Full Stack Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Engineer", "Developer"],
+        strings: ["Software Engineer", "Java-Full Stack Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
